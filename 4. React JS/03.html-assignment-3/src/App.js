2cdc620bcdc6d85861f8e8dc4a6dee399f0718ad
ibtasam116 <ibtasam116@gmail.com>
2023-07-02 18:52:05 +0500

    New app

---
+++ 4. React JS/03.html-assignment-3/src/App.js
@@ -0,0 +1,93 @@
+function App() {
+  return (
+    <div className="App">
+      <img src="./Images/images_kktpq7.png" alt="" width="100%" />
+      <table>
+        <tr>
+          <td width="65%" align="center"><b>Ara Profile</b></td>
+        </tr>
+        <tr>
+          <td>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit illo aliquid modi iusto
+              dignissimos, ullam a quis, molestiae gon veniam perferendis, natus error delectus esse saepe
+              laudantium accusantium voluptatum quam?Lorem ipsum. Lorem ipsum, dolor sit amet consectetur
+              adipisicing </p>
+          </td>
+          <td>
+            <a href=""><img src="./Images/insta_cf1rkw.jpg" alt="" /></a>
+            <a href=""><img src="./Images/twiter_y8idrd.png" alt="" /></a>
+            <a href=""><img src="./Images/Fb logo.jpeg" alt="" width="30px" /></a><br />
+            <a href="">fb.com/naveed.trainer</a>
+          </td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <td width="25%">
+            <img src="./Images/2_t5rcog.jpg" alt="" width="100%" height="290hv" />
+          </td>
+          <td valign="top" width="40%">
+            <b>Cable Tray</b>
+            <p>Lorem ipsum dolor, sit amet consectetur godsadns adipisicing elit. A provident minima tempore
+              dignissimos?
+              Accusamus rem aliquid aperiam officiis eum impedit quidem۔</p>
+            <p>Lorem ipsum dolor, sit amet consectetur godsadns adipisicing elit. A provident minima tempore
+              dignissimos?
+              Accusamus rem aliquid aperiam officiis eum impedit quidem۔</p>
+            <p>Lorem ipsum dolor, sit amet consectetur godsadns adipisicing elit. A provident minima tempore
+              dignissimos?
+              Accusamus rem aliquid aperiam officiis eum impedit quidem۔</p>
+          </td>
+          <td valign="top">
+            <img src="./Images/1_xewrnv.jpg" alt="" width="80%" height="150hv" />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad soluta nihil cum labore totam non quis
+              quisquam magni iste aut voluptatibus numquam accusantium veniam adipisci neque, recusandae debitis
+              fugit jahsj sjka et!</p>
+          </td>
+        </tr>
+      </table>
+      <table width="93%">
+        <tr>
+          <td>
+            <ul>
+              <li>Ara</li>
+              <li>Profile</li>
+              <li>Sheet</li>
+              <li>170*</li>
+            </ul>
+          </td>
+          <td>
+            <ul>
+              <li>Ara</li>
+              <li>Profile</li>
+              <li>Sheet</li>
+              <li>170*</li>
+            </ul>
+          </td>
+          <td>
+            <ol>
+              <li>Ara</li>
+              <li>Profile</li>
+              <li>Sheet</li>
+              <li>170*</li>
+            </ol>
+          </td>
+        </tr>
+      </table>
+      <br />
+      <h2 align="center">Join Us</h2>
+      <table>
+        <tr>
+          <td width="68%"></td>
+          <td>
+            <form action="">
+
+            </form>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
