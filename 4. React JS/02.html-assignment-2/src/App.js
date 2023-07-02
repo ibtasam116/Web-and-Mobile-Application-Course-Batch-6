022c1181c0b3c02b98fb16b6e7c9f6fe359f188e
ibtasam116 <ibtasam116@gmail.com>
2023-07-02 17:27:36 +0500

    New App

---
+++ 4. React JS/02.html-assignment-2/src/App.js
@@ -0,0 +1,20 @@
+import img from './images/IMG-20221201-WA0003.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Assignment No. 02</h1>
+    <h2>Learn With Naveed Sarwar</h2>
+    <h3>First Class</h3>
+    <h4>Batch-06</h4>
+    <h5>let's start the journey</h5>
+    <h6>Let's Learn and grow together</h6>
+    <a href="https://www.google.com/" target="_blank">Google</a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nam rem dolor dolore, assumenda dignissimos ab
+        veniam officiis omnis impedit quae quam libero magnam eveniet. Libero doloremque quas ea.</p>
+    <img src={img} alt=""></img>
+    </div>
+  );
+}
+
+export default App;
