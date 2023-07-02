5d5153fde49fe7cca084a89ddef559e952444dc9
ibtasam116 <ibtasam116@gmail.com>
2023-07-02 16:19:16 +0500

    Today Commit

--- 4. React JS/01.html-assignment-1/src/App.js
+++ 4. React JS/01.html-assignment-1/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Saylani Web and Mobile - Batch6</h1>
+      <h2>Assignment No. 01</h2>
+      <h3>Learn With Naveed Sarwar</h3>
+      <h4>First Class</h4>
+      <h5>Batch6</h5>
+      <h6>Let's start the journery</h6>
+      <h6>Let's Learn and grew together</h6>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, eum ex. Voluptatem incidunt fugit unde, eius
+        ipsum est suscipit tenetur quas soluta doloremque ex sed doloribus. Sint commodi incidunt ipsa.</p>
     </div>
   );
 }
