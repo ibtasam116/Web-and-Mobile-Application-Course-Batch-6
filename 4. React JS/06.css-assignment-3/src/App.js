a26200e88cca9d047e15cd78a755fd80ce65b4b2
ibtasam116 <ibtasam116@gmail.com>
2023-07-07 15:03:26 +0500

    Code Done

--- 4. React JS/06.css-assignment-3/src/App.js
+++ 4. React JS/06.css-assignment-3/src/App.js
@@ -2,11 +2,17 @@ import "./App.scss"
 
 import "bootstrap/dist/js/bootstrap.bundle"
 
+import Navbar from "./components/Header/Navbar/Navbar";
+
+import Header_Main from "./components/Header/Header_Main/Header_Main";
+
 function App() {
   return (
     <>
-    <h1>Heelo</h1>
-    hi
+      <Navbar />
+      <Header_Main />
+      <h1 className="text-center">Heelo</h1>
+      hi
     </>
   );
 }
---
+++ 4. React JS/06.css-assignment-3/src/components/Header/Header_Main/Header_Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Header_Main.css'
+
+function Header_Main() {
+    return(
+        <h1>Header_Main</h1>
+    )
+}
+
+export default Header_Main;
