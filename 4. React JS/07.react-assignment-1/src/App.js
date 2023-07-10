7b5d6461dfd516ac88c5b3bb701a33fedc823d70
ibtasam116 <ibtasam116@gmail.com>
2023-07-10 12:39:08 +0500

    Today Commit

--- 4. React JS/07.react-assignment-1/src/App.js
+++ 4. React JS/07.react-assignment-1/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
