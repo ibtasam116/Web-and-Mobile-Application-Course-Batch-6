7311d3300cf409a3525137fed5a1a795b6bb23ed
ibtasam116 <ibtasam116@gmail.com>
2023-07-11 15:36:30 +0500

    new chnages

--- 4. React JS/07.react-assignment-1/src/App.js
+++ 4. React JS/07.react-assignment-1/src/App.js
@@ -1,10 +1,13 @@
+import Navbar from './components/navbar/Navbar';
 import './css/styles.css';
 import './css/swiper-bundle.min.css'
-import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/homePage/HomePage';
 
 function App() {
   return (
     <>
+      <Navbar />
+      
       <HomePage />
     </>
   );
