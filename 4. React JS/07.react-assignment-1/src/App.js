5ebe68fb76934c2796dfe9d3e4096e041e8a7ed4
ibtasam116 <ibtasam116@gmail.com>
2023-07-11 11:56:22 +0500

    Code Done

--- 4. React JS/07.react-assignment-1/src/App.js
+++ 4. React JS/07.react-assignment-1/src/App.js
@@ -4,9 +4,9 @@ import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div>
+    <>
       <HomePage />
-      l    </div>
+    </>
   );
 }
 
--- 4. React JS/07.react-assignment-1/src/pages/HomePage/HomePage.js
+++ 4. React JS/07.react-assignment-1/src/pages/HomePage/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
 export default function HomePage() {
     return (
         <>
             <Navbar />
+
+            <main class="main">
+                <Header />
+            </main>
+
         </>
     )
 }
