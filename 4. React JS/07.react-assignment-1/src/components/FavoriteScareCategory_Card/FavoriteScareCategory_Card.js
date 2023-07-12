b69935e9aa60cd0f45925df9e48d5ac5ba40cae9
ibtasam116 <ibtasam116@gmail.com>
2023-07-12 16:05:37 +0500

    Today Commit

--- 4. React JS/07.react-assignment-1/src/components/FavoriteScareCategory_Card/FavoriteScareCategory_Card.js
+++ 4. React JS/07.react-assignment-1/src/components/FavoriteScareCategory_Card/FavoriteScareCategory_Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function FavoriteScareCategory_Card() {
+export default function FavoriteScareCategory_Card(props) {
     return (
         <div className="category__data">
-            <img src="assets/img/category1-img.png" alt="" className="category__img" />
-            <h3 className="category__title">Ghosts</h3>
-            <p className="category__description">Choose the ghosts, the scariest there are.</p>
+            <img {props.img} alt="" className="category__img" />
+            <h3 className="category__title">{props.title}</h3>
+            <p className="category__description">{props.description}</p>
         </div>
     )
 }
--- 4. React JS/07.react-assignment-1/src/pages/favoriteScareCategory/FavoriteScareCategory.js
+++ 4. React JS/07.react-assignment-1/src/pages/favoriteScareCategory/FavoriteScareCategory.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import FavoriteScareCategory_Card from '../../components/FavoriteScareCategory_Card/FavoriteScareCategory_Card'
 
+import categoryImg1 from "../../assets/img/category1-img.png"
+
 export default function FavoriteScareCategory() {
     return (
         <section className="section category">
             <h2 className="section__title">Favorite Scare <br /> Category</h2>
 
             <div className="category__container container grid">
-                <FavoriteScareCategory_Card />
+                <FavoriteScareCategory_Card img={categoryImg1} title="Ghosts" description="Choose the ghosts, the scariest there are."/>
                 <FavoriteScareCategory_Card />
                 <FavoriteScareCategory_Card />
             </div>
