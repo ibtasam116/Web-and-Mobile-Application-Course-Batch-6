32efc4566775b34b7f7415d420522c130e0cafa7
ibtasam116 <ibtasam116@gmail.com>
2023-07-13 06:14:05 +0500

    Today Commit

--- 4. React JS/07.react-assignment-1/src/components/trickOrTreat_Card/TrickOrTreat_Card.js
+++ 4. React JS/07.react-assignment-1/src/components/trickOrTreat_Card/TrickOrTreat_Card.js
@@ -6,7 +6,7 @@ export default function TrickOrTreat_Card(props) {
             <img src={props.img} alt="" className="trick__img" />
             <h3 className="trick__title">{props.trick_title}</h3>
             <span className="trick__subtitle">{props.trick_subtitle}</span>
-            <span className="trick__price">$11.99</span>
+            <span className="trick__price">${props.trick_price}</span>
             <button className="button trick__button">
                 <i className='bx bx-cart-alt trick__icon'></i>
             </button>
--- 4. React JS/07.react-assignment-1/src/pages/trickOrTreat/TrickOrTreat.js
+++ 4. React JS/07.react-assignment-1/src/pages/trickOrTreat/TrickOrTreat.js
@@ -14,12 +14,12 @@ export default function TrickOrTreat() {
             <h2 className="section__title">Trick Or Treat</h2>
 
             <div className="trick__container container grid">
-                <TrickOrTreat_Card img={Trick_treatImg1} trick_title="Toffee" trick_subtitle="Candy"/>
-                <TrickOrTreat_Card img={Trick_treatImg2}/>
-                <TrickOrTreat_Card img={Trick_treatImg3}/>
-                <TrickOrTreat_Card img={Trick_treatImg4}/>
-                <TrickOrTreat_Card img={Trick_treatImg5}/>
-                <TrickOrTreat_Card img={Trick_treatImg6}/>
+                <TrickOrTreat_Card img={Trick_treatImg1} trick_title="Toffee" trick_subtitle="Candy" trick_price="11.99" />
+                <TrickOrTreat_Card img={Trick_treatImg2} trick_title="Bone" trick_subtitle="Accessory" trick_price="8.99" />
+                <TrickOrTreat_Card img={Trick_treatImg3} trick_title="Scarecrow" trick_subtitle="Accessory" trick_price="15.99" />
+                <TrickOrTreat_Card img={Trick_treatImg4} trick_title="Candy Cane" trick_subtitle="Candy" trick_price="7.99" />
+                <TrickOrTreat_Card img={Trick_treatImg5} trick_title="Pumpkin" trick_subtitle="Candy" trick_price="19.99" />
+                <TrickOrTreat_Card img={Trick_treatImg6} trick_title="Ghost" trick_subtitle="Accessory" trick_price="17.99" />
             </div>
         </section>
     )
