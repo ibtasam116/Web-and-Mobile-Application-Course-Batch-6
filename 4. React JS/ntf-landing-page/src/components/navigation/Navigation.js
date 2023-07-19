e60c5fc6ccff5135f2bb1da3efcaf3a2c5125b09
ibtasam116 <ibtasam116@gmail.com>
2023-07-19 06:50:05 +0500

    Today Commit

---
+++ 4. React JS/ntf-landing-page/src/components/navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navigation.css'
+export default function navigation() {
+  return (
+    <>
+      <div className='Container-fluid border'>
+        <h1 className=''></h1>
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
