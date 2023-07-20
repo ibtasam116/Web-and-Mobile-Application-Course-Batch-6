4d66182d71e203efe7dde6105b3e401b6b2117dd
ibtasam116 <ibtasam116@gmail.com>
2023-07-20 15:14:00 +0500

    Clean

--- 4. React JS/ntf-landing-page/src/App.js
+++ 4. React JS/ntf-landing-page/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 import Navigation from './components/navigation/Navigation';
 import Hero_section from './sections/hero_section/Hero_section'
 
 function App() {
   return (
     <>
-      {/* <Navigation /> */}
-      <Hero_section />
+      <Navigation />
+      {/* <Hero_section /> */}
     </>
   );
 }
--- 4. React JS/ntf-landing-page/src/components/navigation/Navigation.js
+++ 4. React JS/ntf-landing-page/src/components/navigation/Navigation.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Navigation.css'
+
+import { PiStorefront } from "react-icons/pi"
 export default function navigation() {
   return (
     <>
-      <div className='Container-fluid border'>
-        <h1 className=''></h1>
-        <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
+      <div id="navigation">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 d-flex">
+              <a href='#'><PiStorefront className='logo'/></a>
+              <h5 id='main-title'>NFT Marketplace</h5>
+            </div>
+            <div className="col-6"></div>
+          </div>
         </div>
       </div>
 
