b363bfe7c9ac96d94edea392150de41ddfa21e9a
ibtasam116 <ibtasam116@gmail.com>
2023-07-21 07:49:33 +0500

    21 July 2023

--- 4. React JS/ntf-landing-page/src/components/navigation/Navigation.js
+++ 4. React JS/ntf-landing-page/src/components/navigation/Navigation.js
@@ -8,11 +8,17 @@ export default function navigation() {
       <div id="navigation">
         <div className="container">
           <div className="row">
-            <div className="col-6 d-flex">
-              <a href='#'><PiStorefront className='logo'/></a>
-              <h5 id='main-title'>NFT Marketplace</h5>
+            <div className="col-6 d-flex align-items-center py-2">
+              <a href='#'><PiStorefront className='logo' /></a>
+              <a><h5 id='main-title' className='text-white mt-2 ms-3'>NFT Marketplace</h5></a>
             </div>
-            <div className="col-6"></div>
+            <div className="col-6 navigation_menus">
+              <ul>
+                <li><a href="#">Marketplace</a></li>
+                <li><a href="#">Rankings</a></li>
+                <li><a href="#">Connect a wallet</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
