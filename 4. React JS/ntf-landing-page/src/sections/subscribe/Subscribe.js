2b56d4741836879b0a6ae049e2913a22d6341ddd
ibtasam116 <ibtasam116@gmail.com>
2023-07-29 11:43:31 +0500

    Good Work

--- 4. React JS/ntf-landing-page/src/sections/subscribe/Subscribe.js
+++ 4. React JS/ntf-landing-page/src/sections/subscribe/Subscribe.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Subscribe.css"
 
 import Button from '../../components/button/Button'
-import { AiOutlineUser } from 'react-icons/ai'
+import { PiEnvelopeSimpleLight } from 'react-icons/pi'
 
 import img from "../../assets/images/subscribe/Photo.png"
 
@@ -12,18 +12,18 @@ export default function Subscribe() {
             <div className="container text-white p-5">
                 <div className="row p-5 row-color">
                     <div className="col-sm-12 col-md-12 col-lg-6 text-center">
-                        <img src={img} alt="" className='img-fluid' />
+                        <img src={img} alt="" className='img-fluid subs-image' />
                     </div>
-                    <div className="col-6 px-5">
-                        <h3 className='main-heading'>Join our weekly digest</h3>
-                        <p>Get exclusive promotions & updates straight to your inbox.</p>
-                        <div className='subscribe_buttons'>
-                            <input type="text" className='new-input' placeholder='Enter your email here' />
+                    <div className="col-sm-12 col-md-12 col-lg-6 px-0 px-md-5 px-lg-0">
+                        <h3 className='subs-heading'>Join our weekly digest</h3>
+                        <p className='subs-para'>Get exclusive promotions & updates straight to your inbox.</p>
+                        <div className='subscribe_buttons text-center'>
+                            <input type="text" placeholder='Enter your email here' />
                             <Button
-                                btnTitle="Sign Up"
+                                btnTitle="Subscribe"
                                 size="btn-padding-lg"
                                 hover="btn-hover"
-                                icon={AiOutlineUser}
+                                icon={PiEnvelopeSimpleLight}
                                 marginEnd="icon-mar-sm" />
                         </div>
                     </div>
