eb0eaf9198327bb92318b871310f0c3633919f2c
ibtasam116 <ibtasam116@gmail.com>
2023-07-29 23:01:55 +0500

    new project

---
+++ 4. React JS/10.todolist-app/src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="flex bg-purple-300">
+      <h1 className="text-blue-900 mx-12 cursor-pointer">Hello World</h1>
+      <h1 className="text-blue-900 mx-4 flex">Hello World</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
