021ec4f3a6bd85ad9aa6f06db9cf26bd369a3f88
ibtasam116 <ibtasam116@gmail.com>
2023-07-30 11:08:46 +0500

    Tailwind

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -1,12 +1,31 @@
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
     <>
-    <div className="flex bg-purple-300">
-      <h1 className="text-blue-900 mx-12 cursor-pointer">Hello World</h1>
-      <h1 className="text-blue-900 mx-4 flex">Hello World</h1>
-    </div>
+      <div className="container mx-auto">
+        <div className='grid-rows-{n}'>
+
+          <div className='columns-3'>
+            <div className="flex bg-red-200">
+              <h1 className="text-blue-200 mx-12 cursor-pointer">Hello World</h1>
+              <h1 className="text-blue-900 mx-4">Hello World</h1>
+            </div>
+          </div>
+          <div className='columns-3'>
+            <div className="flex bg-red-200">
+              <h1 className="text-blue-900 mx-12 cursor-pointer">Hello World</h1>
+              <h1 className="text-blue-900 mx-4">Hello World</h1>
+            </div>
+          </div>
+          <div className='columns-3'>
+            <div className="flex bg-red-200">
+              <h1 className="text-blue-900 mx-12 cursor-pointer">Hello World</h1>
+              <h1 className="text-blue-900 mx-4">Hello World</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
