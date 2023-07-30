ccd7f9cccfcd2fa07e23c906e79c9519fb0edd46
ibtasam116 <ibtasam116@gmail.com>
2023-07-30 21:48:41 +0500

    Night

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -3,16 +3,13 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className='h-[100vh] pt-[5%] bg-purple-700'>
-        <div className="flex justify-center">
-          <div className="bg-white w-[90%] sm:w-[70%] md:w-[40%] max-h-[70vh] p-6">
-            <div className='bg-purple-700 max-h-[70vh] shadow-2xl p-5'>
-              <input type="text" className='outline-none w-[75%] p-2'/>
-              <button className='bg-white w-[21%] ms-3 p-2'>Add Task</button>
-              {/* <div className=' py-[2%] px-[5%]'>
-              <input type="text" className='py-1 ps-2 w-60 outline-none ' placeholder='Add new task' />
-              <button className='text-center bg-white text-purple-700 font-bold uppercase ms-3 px-4 py-1 text-[20px]'>Add Task</button>
-            </div> */}
+      <div className='h-[100vh] bg-purple-700'>
+
+        <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4'>
+          <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 p-6'>
+            <div className='bg-purple-700 p-4 text-center'>
+              <input type="text" className='w-[70%] p-2'/>
+              <button className='bg-white w-[26%] p-2 ms-3'>Add TAsk</button>
             </div>
           </div>
         </div>
