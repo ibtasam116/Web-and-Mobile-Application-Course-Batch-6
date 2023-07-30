b93abed244285b78d7a899ec5803163b578b36f5
ibtasam116 <ibtasam116@gmail.com>
2023-07-30 17:31:32 +0500

    Files

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -3,26 +3,11 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="container mx-auto">
-        <div className='grid-rows-{n}'>
-
-          <div className='columns-3'>
-            <div className="flex bg-red-200">
-              <h1 className="text-blue-200 mx-12 cursor-pointer">Hello World</h1>
-              <h1 className="text-blue-900 mx-4">Hello World</h1>
-            </div>
-          </div>
-          <div className='columns-3'>
-            <div className="flex bg-red-200">
-              <h1 className="text-blue-900 mx-12 cursor-pointer">Hello World</h1>
-              <h1 className="text-blue-900 mx-4">Hello World</h1>
-            </div>
-          </div>
-          <div className='columns-3'>
-            <div className="flex bg-red-200">
-              <h1 className="text-blue-900 mx-12 cursor-pointer">Hello World</h1>
-              <h1 className="text-blue-900 mx-4">Hello World</h1>
-            </div>
+      <div className='h-96 py-48 bg-purple-700'>
+        <div className="container m-auto flex justify-center">
+          <div className="card border-white p-8 border-8 bg-purple-700-50 flex">
+            <input type="text" className='py-1 ps-2 w-60 outline-none ' placeholder='Add new task'/>
+            <button className='text-center bg-white text-purple-700 font-bold uppercase ms-3 px-4 py-1 text-[20px]'>Add Task</button>
           </div>
         </div>
       </div>
