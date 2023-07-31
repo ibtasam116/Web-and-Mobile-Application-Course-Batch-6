2677920ea00ff34dcc9144a6ca87600b29afe1a1
ibtasam116 <ibtasam116@gmail.com>
2023-07-31 09:46:17 +0500

    JU

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 
 
 function App() {
+
   const [newItem, setNewItem] = useState("");
-  const [todo, setTodo] = useState([])
-  const handleClick = () => {
-    if (value !== "" && todo !== "") {
-      setTodo(...todo, value)
-      setValue('');
+  const [items, setItems] = useState([])
+  function addItem() {
+    if (!newItem) {
+      alert("Please Enter Items")
+      return;
     }
+    console.log(newItem)
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+    setNewItem("");
+    console.log(items)
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+    console.log(id);
   }
 
 
@@ -20,18 +36,18 @@ function App() {
           <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 p-6'>
             <div className='bg-purple-700 p-4 text-center'>
               <input onChange={e => setNewItem(e.target.value)} value={newItem} type="text" className='w-[70%] p-2' />
-              <button onClick={handleClick} className='bg-white w-[26%] p-2 ms-3'>Add TAsk</button>
+              <button onClick={() => addItem()} className='bg-white w-[26%] p-2 ms-3'>Add TAsk</button>
             </div>
           </div>
         </div>
         <div>
-          <ol>
-            {todo.map((todos) => <li key={hi}>{todos}</li>)}
-          </ol>
-          {/* 
-          <ol>
-            {todo.map((todos => <li>{todos}</li>))}
-          </ol> */}
+          <ul>
+            {items.map(item => {
+              return (
+                <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)} className='p-2 bg-white'>X</button></li>
+              )
+            })}
+          </ul>
         </div>
       </div>
     </>
