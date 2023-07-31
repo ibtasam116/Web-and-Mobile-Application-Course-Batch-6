48dde6c1b25139d4e5f3f5c2f37564f1aa748f64
ibtasam116 <ibtasam116@gmail.com>
2023-07-31 11:26:54 +0500

    hi

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -32,22 +32,32 @@ function App() {
   return (
     <>
       <div className='h-[100vh] bg-purple-700'>
-        <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 pt-8'>
           <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 p-6'>
-            <div className='bg-purple-700 p-4 text-center'>
-              <input onChange={e => setNewItem(e.target.value)} value={newItem} type="text" className='w-[70%] p-2' />
-              <button onClick={() => addItem()} className='bg-white w-[26%] p-2 ms-3'>Add TAsk</button>
+            <div className='bg-purple-700 p-4 text-center shadow-lg'>
+              <input onChange={e => setNewItem(e.target.value)} value={newItem} type="text" className='w-[70%] p-2 outline-none' placeholder='Add new task'/>
+              <button onClick={() => addItem()} className='bg-white w-[26%] p-2 ms-3 hover:bg-orange-400 hover:text-white'>Add Task</button>
             </div>
           </div>
         </div>
-        <div>
-          <ul>
-            {items.map(item => {
-              return (
-                <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)} className='p-2 bg-white'>X</button></li>
-              )
-            })}
-          </ul>
+        <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 pt-8'>
+          <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 px-6 pb-6'>
+            <h3 className='text-center font-serif text-[30px] hover:underline transi'>TODOS</h3>
+            <div className='bg-purple-700 p-4 text-center shadow-lg'>
+              <ul>
+                {items.map(item => {
+                  return (
+                    <>
+                      <li key={item.id} className='bg-white w-[70%] text-left py-2 inline-block my-1'>{item.value}
+                      </li>
+                      <button onClick={() => deleteItem(item.id)} className='py-2 px-3 mx-2 bg-white hover:bg-red-700 hover:text-white'>Delete</button>
+                      <button onClick={() => deleteItem(item.id)} className='py-2 px-5 bg-white hover:bg-green-700 hover:text-white'>Edit</button>
+                    </>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
