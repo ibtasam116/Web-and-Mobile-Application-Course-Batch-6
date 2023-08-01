42b4e4213be8fc4702c1e541af8ecddd7491ab6a
ibtasam116 <ibtasam116@gmail.com>
2023-08-01 15:12:14 +0500

    Session  Miss

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <>
       <div className='h-[100vh] bg-purple-700'>
-        <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 pt-8'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-4 grid-cols-1 p-3 pt-8'>
           <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 p-6'>
             <div className='bg-purple-700 p-4 text-center shadow-lg'>
               <input onChange={e => setNewItem(e.target.value)} value={newItem} type="text" className='w-[70%] p-2 outline-none' placeholder='Add new task'/>
@@ -40,7 +40,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className='grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 pt-8'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-4 grid-cols-1 p-3 pt-8'>
           <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 px-6 pb-6'>
             <h3 className='text-center font-serif text-[30px] hover:underline transi'>TODOS</h3>
             <div className='bg-purple-700 p-4 text-center shadow-lg'>
