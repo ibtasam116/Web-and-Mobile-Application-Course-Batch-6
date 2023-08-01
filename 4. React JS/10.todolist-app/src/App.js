c92e4c7d975a7d2e61420c1a980dd01458c3380f
ibtasam116 <ibtasam116@gmail.com>
2023-08-01 15:19:30 +0500

    Nice Work

--- 4. React JS/10.todolist-app/src/App.js
+++ 4. React JS/10.todolist-app/src/App.js
@@ -33,10 +33,10 @@ function App() {
     <>
       <div className='h-[100vh] bg-purple-700'>
         <div className='grid lg:grid-cols-4 md:grid-cols-4 grid-cols-1 p-3 pt-8'>
-          <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-2 sm:col-end-4 p-6'>
+          <div className='bg-white lg:col-start-2 lg:col-end-4 md:col-start-2 md:col-end-4 sm:col-start-1 sm:col-end-4 p-6'>
             <div className='bg-purple-700 p-4 text-center shadow-lg'>
               <input onChange={e => setNewItem(e.target.value)} value={newItem} type="text" className='w-[70%] p-2 outline-none' placeholder='Add new task'/>
-              <button onClick={() => addItem()} className='setBtn bg-white w-[26%] p-2 ms-3 hover:bg-orange-400 hover:text-white'>Add Task</button>
+              <button onClick={() => addItem()} className='setBtn md:text-[15px] lg:text-lg bg-white md:w-[26%] lg:w-[26%] mt-2  p-2 ms-3 hover:bg-orange-400 hover:text-white'>Add Task</button>
             </div>
           </div>
         </div>
