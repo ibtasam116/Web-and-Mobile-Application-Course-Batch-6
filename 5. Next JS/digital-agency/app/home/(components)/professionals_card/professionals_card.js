e5c5a5e853171b2c3fefa9b20967399a10fd953a
ibtasam116 <ibtasam116@gmail.com>
2023-08-29 17:36:44 +0500

    Good

---
+++ 5. Next JS/digital-agency/app/home/(components)/professionals_card/professionals_card.js
@@ -0,0 +1,10 @@
+
+export default function Professionals_card(props) {
+    return (
+        <>
+            <div className='flex justify-center'>
+                <div className={`bg-[#E7E7E7] w-72 sm:w-full h-96 md:h-64 lg:h-96 hover:shadow-2xl transition-all flex items-end justify-center text-white text-xl font-bold ${props.dis}`}></div>
+            </div>
+        </>
+    )
+}
--- 5. Next JS/digital-agency/app/home/home_sections/professionals/professionals.js
+++ 5. Next JS/digital-agency/app/home/home_sections/professionals/professionals.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import Professionals_card from '../../(components)/professionals_card/professionals_card'
 
 export default function Professionals() {
     return (
         <>
-        <section className='py-8 sm:mb-32 bg-[#F2F9FF]'>
+            <section className='py-10 sm:mb-32 bg-[#F2F9FF]'>
+                <div className='container mx-auto px-[5%] md:px-[10%]'>
+                    <div className='text-center'>
+                        <h3 className='text-[#2370C8]'>OUR TEAM</h3>
+                        <p className='text-[#262A4E] text-sm sm:text-xl md:text-2xl font-bold'>Our top dedicated professionals.</p>
+                    </div>
+                    <div className='grid sm:grid-cols-2 md:grid-cols-3  gap-5 my-8'>
+                        <Professionals_card />
+                        <Professionals_card />
+                        <Professionals_card dis="block sm:hidden md:block"/>
+                    </div>
 
-        </section>
+                </div>
+            </section>
         </>
     )
 }
