1a2d835759ca38c4beee0c6ffe9896f4ee0d7aee
ibtasam116 <ibtasam116@gmail.com>
2023-08-30 11:21:28 +0500

    Good

---
+++ 5. Next JS/digital-agency/app/home/home_sections/clients/clients.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Clients() {
+    return (
+        <>
+            <section className='max-h-[100#] bg-[#3782d7e6] py-20'>
+                <div className='container mx-auto px-[5%] md:px-[10%] text-white'>
+                    <h1 className='text-3xl font-bold text-center'>We’re trusted by clients.</h1>
+                    <div className='grid grid-cols-2'>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
--- 5. Next JS/digital-agency/app/home/page.js
+++ 5. Next JS/digital-agency/app/home/page.js
@@ -1,3 +1,4 @@
+import Clients from "./(components)/clients/clients";
 import Digital_services from "./home_sections/digital_services/digital_services";
 import Home_about from "./home_sections/home_about/home_about";
 import Home_header from "./home_sections/home_header/home_header";
@@ -16,6 +17,7 @@ export default function Home() {
             <Latest_work />
             <Professionals />
             <Plans />
+            <Clients/>
         </>
     )
 }
