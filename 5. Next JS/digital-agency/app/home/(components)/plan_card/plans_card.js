3d87aac4e2985eae857f3d9391135b7b469cb025
ibtasam116 <ibtasam116@gmail.com>
2023-09-01 16:13:07 +0500

    goof

--- 5. Next JS/digital-agency/app/home/(components)/plan_card/plans_card.js
+++ 5. Next JS/digital-agency/app/home/(components)/plan_card/plans_card.js
@@ -1,4 +1,5 @@
 import Button from '@/app/(components)/button/button'
+import Image from 'next/image'
 import React from 'react'
 
 export default function Plans_card(props) {
@@ -9,16 +10,7 @@ export default function Plans_card(props) {
 
                     <h2 className='text-xl font-bold'>Basic Plan</h2>
                     <div className='py-5'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="91" height="87" viewBox="0 0 91 87" fill="none">
-                            <circle cx="43.2071" cy="43.2071" r="43.2071" fill="#D6EDFF" />
-                            <circle cx="43.5" cy="5.5" r="3.1" stroke="#4A8EDD" stroke-width="0.8" />
-                            <circle cx="76.5" cy="79.5" r="3.1" stroke="#4A8EDD" stroke-width="0.8" />
-                            <circle cx="79" cy="11" r="2" fill="#3F8CE3" />
-                            <line x1="42.888" y1="75.3931" x2="42.888" y2="81.6131" stroke="#3782D7" stroke-width="0.8" />
-                            <line x1="46.2201" y1="78.2809" x2="40" y2="78.2809" stroke="#3782D7" stroke-width="0.8" />
-                            <line x1="85.7119" y1="19.8372" x2="90.0425" y2="25.1878" stroke="#3782D7" stroke-width="0.8" />
-                            <line x1="90.2828" y1="19.6403" x2="85.4155" y2="24.5076" stroke="#3782D7" stroke-width="0.8" />
-                        </svg>
+                        <Image src={props.imgSrc} className='rounded-full' width={100} height={100}/>
                     </div>
                     <div>
                         <h2>
@@ -34,9 +26,35 @@ export default function Plans_card(props) {
                         SEO Marketing <br />
                         Business Analysis
                     </div>
-                    <Button btnTitle="Start Now" bgColor="hover:bg-white text-white hover:text-[#2370C8] font-bold"/>
+                    <Button btnTitle="Start Now" bgColor="hover:bg-white text-white hover:text-[#2370C8] font-bold" />
                 </div>
             </div>
         </>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <svg xmlns="http://www.w3.org/2000/svg" width="91" height="87" viewBox="0 0 91 87" fill="none">
+                            <circle cx="43.2071" cy="43.2071" r="43.2071" fill="#D6EDFF" />
+                            <circle cx="43.5" cy="5.5" r="3.1" stroke="#4A8EDD" stroke-width="0.8" />
+                            <circle cx="76.5" cy="79.5" r="3.1" stroke="#4A8EDD" stroke-width="0.8" />
+                            <circle cx="79" cy="11" r="2" fill="#3F8CE3" />
+                            <line x1="42.888" y1="75.3931" x2="42.888" y2="81.6131" stroke="#3782D7" stroke-width="0.8" />
+                            <line x1="46.2201" y1="78.2809" x2="40" y2="78.2809" stroke="#3782D7" stroke-width="0.8" />
+                            <line x1="85.7119" y1="19.8372" x2="90.0425" y2="25.1878" stroke="#3782D7" stroke-width="0.8" />
+                            <line x1="90.2828" y1="19.6403" x2="85.4155" y2="24.5076" stroke="#3782D7" stroke-width="0.8" />
+                        </svg> */}
--- 5. Next JS/digital-agency/app/home/home_sections/plans/plans.js
+++ 5. Next JS/digital-agency/app/home/home_sections/plans/plans.js
@@ -1,42 +1,40 @@
-import React from 'react'
 import Plans_card from '../../(components)/plan_card/plans_card'
+
+import plan1 from "@/public/imgs/plan/plan (1).jpg"
+import plan2 from "@/public/imgs/plan/plan (2).jpg"
+import plan3 from "@/public/imgs/plan/plan (3).jpg"
 
 export default function Plans() {
     return (
         <>
-            <section>
+            <section className='py-10 sm:mb-32'>
+
                 <div className='container mx-auto px-[5%] md:px-[10%]'>
-                    <div className='grid grid-cols-2 md:grid-cols-4'>
-                        <div className='col-span-3'>
-                            <h3 className='text-[#2370C8] text-md'>OUR PRICING</h3>
-                            <h1 className='text-[#262A4E] text-xl sm:text-3xl mt-3 font-bold'>Choose A Plan That’s Right For You</h1>
+                  
+                   <div className='grid grid-cols-2 md:grid-cols-12'>
+                       
+                        <div className='col-span-12 md:col-span-9 text-center md:text-start'>
+                            <h3 className='text-[#2370C8] text-lg uppercase hover:underline underline-offset-4 transition-all inline cursor-default'>OUR PRICING</h3>
+                            <h1 className='text-[#262A4E] text-sm sm:text-2xl lg:text-3xl pt-1 sm:pt-2 font-bold'>Choose A Plan That’s Right For You</h1>
                         </div>
-                        <div className='flex items-center justify-center sm:justify-end'>
-                            <span className='text-[#2370C8]'>Monthly</span>
-                            <svg className='mx-3' xmlns="http://www.w3.org/2000/svg" width="60" height="40" viewBox="0 0 60 40" fill="none">
-                                <rect x="8" y="8" width="52" height="23" rx="10" fill="#2370C8" />
-                                <g filter="url(#filter0_d_173_191)">
-                                    <circle cx="20" cy="19" r="13" fill="white" />
-                                </g>
-                                <defs>
-                                    <filter id="filter0_d_173_191" x="0" y="0" width="40" height="40" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                                        <feOffset dy="1" />
-                                        <feGaussianBlur stdDeviation="3.5" />
-                                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.42 0" />
-                                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_173_191" />
-                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_173_191" result="shape" />
-                                    </filter>
-                                </defs>
-                            </svg>
-                            <span className='text-[#262A4E]'>Yearly</span>
+
+                        <div className='col-span-12 md:col-span-3 flex items-center justify-center md:justify-end mt-3 md:mt-0'>
+
+                            <span class="text-sm font-bold text-[#2370C8]">Monthly</span>
+                            <label class="relative inline-flex items-center cursor-pointer mx-3">
+                                <input type="checkbox" value="" class="sr-only peer" />
+                                <div class="w-11 h-6 bg-gray-200 rounded-full dark:bg-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                            </label>
+                            <span class="text-sm font-bold text-[#262A4E]">Yearly</span>
+
                         </div>
+
                     </div>
+
                     <div className='grid sm:grid-cols-2 md:grid-cols-3  gap-5 my-8'>
-                        <Plans_card price="50" />
-                        <Plans_card price="99" />
-                        <Plans_card price="150" />
+                        <Plans_card imgSrc={plan1} price="50" />
+                        <Plans_card imgSrc={plan2} price="99" />
+                        <Plans_card imgSrc={plan3} price="150" />
                     </div>
 
                 </div>
