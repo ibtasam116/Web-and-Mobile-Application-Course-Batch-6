f0dcf8ed06436683f16d20e455ec184fe750bfc3
ibtasam116 <ibtasam116@gmail.com>
2023-09-05 22:05:30 +0500

    Nice

--- 5. Next JS/digital-agency/app/layout.js
+++ 5. Next JS/digital-agency/app/layout.js
@@ -19,6 +19,10 @@ export default function RootLayout({ children }) {
 
         <Footer />
 
+
+        {/* Tailwind JS CDN */}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+
       </body>
     </html>
   )
