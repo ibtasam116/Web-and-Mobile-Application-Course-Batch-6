f60669afe1fb7ed30ca796227f787252df635a93
ibtasam116 <ibtasam116@gmail.com>
2023-09-05 23:31:47 +0500

    Good

--- 5. Next JS/todo-app/app/page.js
+++ 5. Next JS/todo-app/app/page.js
@@ -1,85 +1,116 @@
-import React from 'react'
-
+"use client"
 export default function Home() {
 
-  // const studentData = () => [{
-  //   serialNo: 1,
-  //   studentName: "Ali",
-  //   rollNo: 12,
-  //   class: "9th"
-  // },
-  // {
-  //   serialNo: 1,
-  //   studentName: "Ali",
-  //   rollNo: 12,
-  //   class: "9th"
-  // },
-  // {
-  //   serialNo: 1,
-  //   studentName: "Ali",
-  //   rollNo: 12,
-  //   class: "9th"
-  // }
+  const userName = ["Ali", "Naeem", "Umair"]
 
-  // ]
+  const student = [{
+    studentName: "ALi",
+    rollNo: 12,
+    section: "A",
+  },
+  {
+    studentName: "Hamza",
+    rollNo: 3,
+    section: "E",
+  },
+  {
+    studentName: "Ibtasam",
+    rollNo: 6,
+    section: "D",
+  },
+  {
+    studentName: "Ibtasam",
+    rollNo: 6,
+    section: "D",
+  }]
+
+  const deleteHandler = () => {
+
+  }
+
+
+  const todos = [{
+    task: "Work",
+    time: "9 PM",
+  },
+  {
+    task: "Work",
+    time: "9 PM",
+  },
+  {
+    task: "Work",
+    time: "9 PM",
+  },]
 
   return (
     <>
 
+      <button>{2 !== 2 ? "true" : "False"}</button>
 
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-          </a>
-          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <table>
+        <tr>
+          <th>No.</th>
+          <th>Task</th>
+          <th>Time</th>
+          <th>Delete</th>
+          <th>Update</th>
+        </tr>
+        {
+          todos.map((todo, i) => {
+            return (
+              <>
+                <tr>
+                  <td>{i + 1}</td>
+                  <td>{todo.task}</td>
+                  <td>{todo.time}</td>
+                  <td><button>Delete</button></td>
+                  <td><button>Update</button></td>
+                </tr>
+              </>
+            )
+          })
+        }
+      </table >
 
       <h1 className='text-center text-xl sm:text-2xl lg:text-5xl font-bold my-3'>Todo App</h1>
-      {/* 
-      <table className='border-2 border-gray-800'>
+
+      {
+        userName.map((item) => {
+          return (
+            <>{userName}</>
+          )
+        })
+      }
+
+      <hr />
+
+
+      <table>
         <tr>
           <th>#</th>
           <th>Name</th>
           <th>Roll No.</th>
-          <th>Class</th>
+          <th>Section</th>
+          <th>Delete</th>
         </tr>
-        {studentData.map((items) => {
-          return (
-            <tr>
-              <td>{items.serialNo}</td>
-            </tr>
-          )
-        })}
+        {
+          student.map((student, i) => {
+            return (
+              <>
+                <tr>
+                  <td>{i}</td>
+                  <td>{student.studentName}</td>
+                  <td>{student.rollNo}</td>
+                  <td>{student.section}</td>
+                  <td><button onClick={deleteHandler} >Delete</button></td>
+                </tr>
+              </>
+            )
+          })
+        }
+      </table>
 
-      </table> */}
+
 
     </>
   )
