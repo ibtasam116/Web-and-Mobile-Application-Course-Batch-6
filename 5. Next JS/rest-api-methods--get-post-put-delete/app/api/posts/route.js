0c71bad57ea9caacb42399d078fed6180826cace
ibtasam116 <ibtasam116@gmail.com>
2023-10-01 09:50:57 +0500

    Morning

--- 5. Next JS/rest-api-methods--get-post-put-delete/app/api/posts/route.js
+++ 5. Next JS/rest-api-methods--get-post-put-delete/app/api/posts/route.js
@@ -1,3 +1,10 @@
+// Three methods of Send data
+/*
+1. Body
+2. Param
+3. Query
+*/
+
 import { NextResponse } from "next/server"
 
 var posts = [
@@ -19,9 +26,36 @@ export const GET = () => {
     return NextResponse.json(posts);
 }
 
+// body method
 export const POST = async (req) => {
     const body = await req.json()
     posts.push(body);
     // return NextResponse.json({ message: "Post Created" })
     return NextResponse.json(posts)
+}
+
+// Query method
+export const DELETE = (req) => {
+    const id = new URL(req.url).searchParams.get("id")
+    console.log("Id", id);
+    posts = posts.filter((post) => post.id != id)
+    // return NextResponse.json({ message: "Post Deleted", id: id })
+    return NextResponse.json(posts)
+}
+
+// body method
+
+export const PUT = async (req) => {
+    const body = await req.json();
+
+    posts = posts.map((post) => {
+        if (post.id == body.id) {
+            return body
+        }
+        else {
+            return post
+        }
+    })
+
+    return NextResponse.json({ Message: "Post Updated" })
 }
